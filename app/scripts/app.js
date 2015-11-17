9df14bc90bb674a781a6715249945b40d43b5e70
David Enke <david.enke@zalari.de>
2015-11-18 00:46:21 +0100

    initialized angular

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dekombinatdelikatwwwApp
+ * @description
+ * # dekombinatdelikatwwwApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('dekombinatdelikatwwwApp', [
+    'ngAnimate',
+    'ngMessages',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name dekombinatdelikatwwwApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the dekombinatdelikatwwwApp
+ */
+angular.module('dekombinatdelikatwwwApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
