9a5880ea733d617d2e8d82082293bb16651ee9ae
David Enke <post@davidenke.de>
2015-11-19 02:37:10 +0100

    added config

---
+++ app/scripts/app.config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name dekombinatdelikatwww
+ * @description
+ * # dekombinatdelikatwww
+ *
+ * Config of the application.
+ */
+angular
+    .module('dekombinatdelikatwww')
+    .constant('config', {});
