7ba8df63c558353341bdfbdd6151da2fe265ad78
David Enke <david.enke@zalari.de>
2015-11-22 03:29:09 +0100

    facebook message styling via filter

---
+++ app/scripts/filters/formatFacebook.filter.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name de.kombinatdelikat.www.filter:formatFacebook
+ * @description
+ * # formatFacebook
+ * Filter to format Facebook posts to html
+ */
+angular
+    .module('de.kombinatdelikat.www')
+    .filter('formatFacebook', function () {
+        return function(text) {
+            return text
+                .replace(/\*+\s*(.*)[\r?\n|\r]*/gm, "<li>$1</li>")
+                .replace(/\r?\n|\r/g, "\r<br>");
+        };
+    });
