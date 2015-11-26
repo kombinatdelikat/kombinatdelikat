e65a3ebffc295fd158f0bb7131b5d3a78c3e5c11
David Enke <david.enke@zalari.de>
2015-11-26 10:57:44 +0100

    finished google maps

--- app/scripts/controllers/locations.js
+++ app/scripts/controllers/locations.js
@@ -12,22 +12,28 @@ angular
     .controller('LocationsCtrl', ['$scope', '$timeout', 'cfpLoadingBar', function ($scope, $timeout, cfpLoadingBar) {
         var _resetLoader = function () {
                 cfpLoadingBar.start();
-                cfpLoadingBar.set(0.02);
+            },
+            _openMarker = function (marker, eventType, model) {
+                var elems = angular.element(document.querySelectorAll('.marker:not(#marker-' + model.id + ')')),
+                    elem = angular.element(document.getElementById('marker-' + model.id));
+
+                elems.removeClass('active');
+                elem.toggleClass('active');
             },
             _init = function () {
                 _resetLoader();
 
                 // set page meta
-                $scope.$parent.meta = {
-                    title: 'Orte - Kombinat Delikat',
+                $scope.$root.meta = {
+                    title: 'Probieren - Kombinat Delikat',
                     description: ''
                 };
 
                 // set maps data
                 $scope.model = {
                     center: {
-                        latitude: 51.06879,
-                        longitude: 13.74312
+                        latitude: 51.06908,
+                        longitude: 13.75537
                     },
                     zoom: 16,
                     options: {
@@ -66,16 +72,7 @@ angular
                                         "visibility": "on"
                                     },
                                     {
-                                        "color": "#000000"
-                                    }
-                                ]
-                            },
-                            {
-                                "featureType": "water",
-                                "elementType": "all",
-                                "stylers": [
-                                    {
-                                        "lightness": -20
+                                        "color": "#ffffff"
                                     }
                                 ]
                             }
@@ -94,18 +91,73 @@ angular
                     markers: [
                         {
                             id: 1,
-                            title: 'test',
-                            latitude: 51.06879,
-                            longitude: 13.74312,
+                            title: 'Kombinat Delikat Fensterverkauf',
+                            coords: {
+                                latitude: 51.06908,
+                                longitude: 13.75537
+                            },
                             options: {
-                                labelContent: 'Test',
-                                labelAnchor: '36 61',
-                                labelClass: 'label',
-                                labelInBackground: false
+                                content: '<div id="marker-1" class="marker"><div><h2>Fensterverkauf</h2><p>Nach Angebot,<br>immer Freitags<br><strong>18:00 - 20:00 Uhr</strong></p></div></div>',
+                                shadow: false,
+                                flat: true
+                            }
+                        },
+                        {
+                            id: 2,
+                            title: 'Sankt Pauli Tagesbar und Restaurant',
+                            coords: {
+                                latitude: 51.07512,
+                                longitude: 13.75033
+                            },
+                            options: {
+                                content: '<div id="marker-2" class="marker"><div><h2>Sankt Pauli</h2><p>Pastrami-Sandwich</p></div></div>',
+                                shadow: false,
+                                flat: true
+                            }
+                        },
+                        {
+                            id: 3,
+                            title: 'Kochbox',
+                            coords: {
+                                latitude: 51.066709,
+                                longitude: 13.754139
+                            },
+                            options: {
+                                content: '<div id="marker-3" class="marker"><div><h2>Kochbox</h2><p>Wechselnde Auswahl an Bratwurst</p></div></div>',
+                                shadow: false,
+                                flat: true
+                            }
+                        }
+                    ],
+                    markers2: [
+                        {
+                            id: 4,
+                            title: 'Kombinat Delikat Fensterverkauf',
+                            coords: {
+                                latitude: 51.06908,
+                                longitude: 13.75537
+                            }
+                        },
+                        {
+                            id: 5,
+                            title: 'Sankt Pauli Tagesbar und Restaurant',
+                            coords: {
+                                latitude: 51.07512,
+                                longitude: 13.75033
+                            }
+                        },
+                        {
+                            id: 6,
+                            title: 'Kochbox',
+                            coords: {
+                                latitude: 51.066709,
+                                longitude: 13.754139
                             }
                         }
                     ]
                 };
+
+                $scope.openMarker = _openMarker;
             };
 
         _resetLoader();
