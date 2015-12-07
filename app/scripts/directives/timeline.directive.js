e69f1c5a638ba72568eefa19bf95513928c70198
David Enke <david.enke@zalari.de>
2015-12-07 09:20:40 +0100

    tried to add padding to realign overlapping items

--- app/scripts/directives/timeline.directive.js
+++ app/scripts/directives/timeline.directive.js
@@ -15,21 +15,37 @@ angular
                 // align timeline items left or right
                 var alignItems = function () {
                         // http://stackoverflow.com/a/8191333/1146207
-                        var left_column_height = 0;
-                        var right_column_height = 90;
-                        var items = element.children();
+                        var padding_range = 20,
+                            left_column_height = 0,
+                            right_column_height = 90,
+                            last_use = false,
+                            items = element.children();
                         for (var i = 1; i < items.length; ++i) {
                             var item = angular
                                 .element(items[i])
-                                .removeClass('left right');
+                                .removeClass('left right spaced');
 
+                            // determine side
                             if (left_column_height > right_column_height) {
+                                last_use = false;
                                 right_column_height += item.addClass('right').prop('offsetHeight');
                             }
-                            /*else if (left_column_height == right_column_height) {}*/
                             else {
+                                last_use = true;
                                 left_column_height += item.addClass('left').prop('offsetHeight');
                             }
+
+                            // add spacing to prevent overlapping
+/*                            if (left_column_height > right_column_height - padding_range / 2
+                                && left_column_height < right_column_height + padding_range / 2) {
+                                if (last_use) {
+                                    left_column_height+= 10;
+                                } else {
+                                    right_column_height+= 10;
+                                }
+                                item.addClass('spaced');
+                                console.log(item[0].className)
+                            }*/
                         }
                     },
                 // load event listener to force re-aligning items
